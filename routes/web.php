01c2969a28deaee238971c931b08a893939a4722
Andras Bacsai <andras.bacsai@gmail.com>
2024-06-13 13:20:25 +0200

    chore: Add routes for retrieving users and caching data

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\Cache;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/users', function () {
+    $users = User::all();
+    return $users;
+});
+Route::get('/cache', function () {
+    Cache::put('key', 'value', 10);
+    return Cache::get('key');
+});
+
